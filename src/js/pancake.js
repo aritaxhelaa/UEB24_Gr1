45e8812f768b1b11ab39796e458b0a94d1045a37
Arita <arita.xhela@student.uni-pr.edu>
2024-12-28 17:03:25 +0100

    .

--- src/js/pancake.js
+++ src/js/pancake.js
@@ -3,10 +3,10 @@ function toggleNutrition() {
     const toggleLink = document.querySelector('.nutrition-toggle');
 
     if (table.style.display === 'none' || table.style.display === '') {
-        table.style.display = 'block'; // Shfaq tabelën
-        toggleLink.textContent = 'Fshih tabelën ushqyese';
+        table.style.display = 'block'; 
+        toggleLink.textContent = 'Hide Nutrition Table';
     } else {
-        table.style.display = 'none'; // Fshih tabelën
-        toggleLink.textContent = 'Shfaq tabelën ushqyese';
+        table.style.display = 'none'; 
+        toggleLink.textContent = 'Show Nutrition Table';
     }
 }
