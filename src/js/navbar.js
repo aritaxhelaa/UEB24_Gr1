33bb5407acb29afb0085b11a8da60cfc515f95b3
ErionMeh <erionm462@gmail.com>
2025-01-03 23:26:40 +0100

    Navbar error fix

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,12 +1,21 @@
-async function loadNavbar() {
-    try {
-        const response = await fetch('../html/navbar.html');
-        const data = await response.text();
-        document.getElementById('navbar-placeholder').innerHTML = data;
-    } catch (error) {
-        console.error('Error loading the navbar:', error);
-    }
-}
-
-
-document.addEventListener('DOMContentLoaded', loadNavbar);
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarHTML = `
+        <nav class="background">
+            <div class="logo">
+                <p id="logoo"><b id="homee">Home</b><span id="logostyle">Cooking</span></p>
+            </div>
+            <div id="list">
+                <ul class="list">
+                    <li><a href="home.html">HOME</a></li>
+                    <li><a href="recipes.html">RECIPES</a></li>
+                    <li><a href="book.html">BOOK</a></li>
+                    <li><a href="about_us.html">ABOUT US</a></li>
+                    <li><a href="healthWellness.html">HEALTH & WELLNESS</a></li>
+                </ul>
+            </div>
+        </nav>
+        <hr id="hr1">
+    `;
+    
+    document.getElementById('navbar-placeholder').innerHTML = navbarHTML;
+});
